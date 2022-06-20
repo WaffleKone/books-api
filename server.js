f1bc073cec4f4494710777e27f934508eeea091e
Waffle <jaxonjjohnson@gmail.com>
2022-06-20 17:46:43 -0400

    added CORS and mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 
 // Configs
 require('dotenv').config()
@@ -8,7 +9,10 @@ const app = express()
 
 // Middleware
 app.use(express.json())
-
+app.use(cors())
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to mongo on: ', process.env.MONGO_URI)
+})
 // Routes
 app.get('/', (req, res) => {
     res.send('this is the index page')
