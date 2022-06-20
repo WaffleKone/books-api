494af397055c35bd9d4495866f49b25058f8a0bb
Waffle <jaxonjjohnson@gmail.com>
2022-06-20 16:46:38 -0400

    added bookSchema

---
+++ models/books.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const bookSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String},
+    year: {type: Number, required: true},
+    quantity: {type: Number, default: 0},
+    imageURL: {type: String, default: 'http://placehold.it/500x500.png'}
+})
